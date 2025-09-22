4729f13857d82312a8a2fefbed0b6b34c3641865
c22393366 <c22393366@mytudublin.ie>
2025-09-22 22:33:25 +0100

    Prepared 2 scenes. Doors are paintable as tiles

--- Assets/Scripts/game_manager.gd
+++ Assets/Scripts/game_manager.gd
@@ -1,9 +1,11 @@
 extends Node
 
 var starting_area = 1
-var curent_area = 1
+var current_area = 1
 var area_path = "res://Assets/Scenes/Areas/"
 
 func next_level():
 	current_area += 1
 	
+func load_area():
+	pass
