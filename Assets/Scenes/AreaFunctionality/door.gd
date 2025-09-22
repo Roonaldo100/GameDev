2344cce05ee90dbaf1a68a381c99a0b576e9548c
c22393366 <c22393366@mytudublin.ie>
2025-09-22 22:47:05 +0100

    Transition between scenes works. Need to now refactor for persistent player

---
+++ Assets/Scenes/AreaFunctionality/door.gd
@@ -0,0 +1,17 @@
+extends Area2D
+class_name AreaExit
+
+var is_open = true
+
+func _ready():
+	pass
+
+func open():
+	is_open = true
+
+func close():
+	is_open = false
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is PlayerController and is_open:
+		GameManager.next_area()
--- Assets/Scripts/game_manager.gd
+++ Assets/Scripts/game_manager.gd
@@ -4,8 +4,11 @@ var starting_area = 1
 var current_area = 1
 var area_path = "res://Assets/Scenes/Areas/"
 
-func next_level():
+func next_area():
 	current_area += 1
+	var full_path = area_path + "area_" + str(current_area) + ".tscn"
+	get_tree().change_scene_to_file(full_path) #tempory solution before refactoring to allow player change between scenes
+	
 	
 func load_area():
 	pass
