876346b5de8d45d867145f45fcef19d660dfb223
CS++ <C22393366@mytudublin.ie>
2025-09-23 18:46:26 +0100

    enemy animator import

---
+++ Assets/Scripts/enemy_animator.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var enemy_controller : EnemyController
+@export var animation_player : AnimationPlayer 
+@export var sprite : Sprite2D
+
+func _process(delta):
+	if enemy_controller.direction == 1:
+		sprite.flip_h = false
+	elif enemy_controller.direction == -1:
+		sprite.flip_h = true
+		
+	if abs(enemy_controller.velocity.x) > 0:
+		animation_player.play("move")
+	else:
+		animation_player.play("idle")
